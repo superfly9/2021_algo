576664e410d18efcb3f22fe48d104bb4e029d609
송승찬 <superfly09@daum.net>
2021-02-16 09:28:16 +0900

    프로그래머스 L2:기능개발 풀이2

--- programmers/L2/기능개발.js
+++ programmers/L2/기능개발.js
@@ -19,3 +19,31 @@ function solution(progresses, speeds) {
 }
 
 console.log(solution(progresses,speeds))
+
+
+
+function solution2(progresses, speeds) {
+    const answer = [];
+
+    while(speeds.length > 0) {
+        // 개발
+        for(let i in speeds) {
+            if(progresses[i] < 100) {
+                progresses[i] += speeds[i];
+            }
+        }
+
+        // 배포
+        let deploy_count = 0;
+        while(progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            deploy_count++;
+        }
+        if(deploy_count > 0) {
+            answer.push(deploy_count);
+        }
+    }
+
+    return answer;
+}
