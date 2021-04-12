5e2e4c2684e90eae68b11bfc86f13d854b2eec8d
송승찬 <superfly09@daum.net>
2021-04-12 09:59:24 +0900

    wwip 큰수만들기 진행중

--- programmers/L2/0412큰수만들기.js
+++ programmers/L2/0412큰수만들기.js
@@ -1,4 +1,20 @@
+// 1231234 => k =3 , length = 7;
+// last Index = (length -  1 ) - k  (마지막 문자의 인덱스 - 앞에서부터 제거해야할 문자 갯수)
+
+ // 1,2,3
+
+3234
+
+// 
 function solution(number, k) {
-    var answer = '';
+    let answer  = [];
+    for (let i=0;i<number.length-1-k;i++) {
+        answer.push(number[i])
+        if (answer[0] < number[i]) {
+            answer.pop();
+            k-=1;
+            answer
+        }
+    }
     return answer;
 }
