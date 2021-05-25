4fe21c0a782fa886262239709e3e479b33974944
superfly9 <superfly09@daum.net>
2021-05-26 08:57:55 +0900

    0526/카카오/괄호변환

--- programmers/L2/0521괄호변환.js
+++ programmers/L2/0521괄호변환.js
@@ -7,38 +7,36 @@
 //   4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
 //   4-3. ')'를 다시 붙입니다. 
 //   4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.(strChanger)
-//   4-5. 생성된 문자열을 반환합니다
-
-//" ()))((() "	 = > () + ))((()
-//  u = () , v= ))((() => v  = u + v로 다시
-// ))((() => u = ))(( , v = () 
-//  u = 올바르지x => 함수의 종료 
-
 function solution(p) {
-    let left = 0,
-        right = 0;
-        u = '',
-        v = '',
-        answer = ''
-        [u,v] = divde(p)
-        console.log('top:','u:',u,'v:',v)
-        if (p.length === 0 ) return '';
+        if(p=='') return '';
+        let answer = '';
+        let [u,v] = divde(p,'')
         if (checkRight(u)) {
-            u+=solution(v);
+            answer+=u;
+            if (v==='') return answer;
+            answer+=solution(v);
         } else {
-            console.log('before answer:',answer)
-            let my = strChanger(v)
-            console.log('my:',my,'v:',v)
-            answer+='(' + v + ')' + my
-            console.log('answer:',answer)
+            answer+='(' + solution(v) + ')' + strChanger(u)
         }
     return answer;
 }
 
+// p ="()))((()"	
+// u = "()"
+// v = "))((()"
+// 문자열 u가 "올바른 괄호 문자열"이므로 그대로 두고, v에 대해 재귀적으로 수행합니다.
+// 다시 두 문자열 u, v로 분리합니다.
+// u = "))(("
+// v = "()"
+// u가 "올바른 괄호 문자열"이 아니므로 다음과 같이 새로운 문자열을 만듭니다.
+// v에 대해 1단계부터 재귀적으로 수행하면 "()"이 반환됩니다.
+// u의 앞뒤 문자를 제거하고, 나머지 문자의 괄호 방향을 뒤집으면 "()"이 됩니다.
+// 따라서 생성되는 문자열은 "(" + "()" + ")" + "()"이며, 최종적으로 "(())()"를 반환합니다.
+// 처음에 그대로 둔 문자열에 반환된 문자열을 이어 붙이면 "()" + "(())()" = "()(())()"가 됩니다.
+
 const divde = (str)=>{
     let left = 0,
         right = 0;
-
     for (let i = 0;i<str.length;i++) {
         if (str[i]=== '(') {
             left++;
@@ -46,16 +44,14 @@ const divde = (str)=>{
             right++;
         }
         if (right === left) {
-            u = str.substring(0,i+1);
-            v = str.substring(i+1);
-            console.log('u:',u,'v:',v);
-            return [u,v]
+            let first = str.substring(0,i+1);
+            second = str.substring(i+1);
+            return [first,second]
         }
     }
 }
 
 const strChanger = (str)=>{
-    console.log('strChange:',str)
     let result = '';
     for (let i=1;i<str.length-1;i++) {
         if (str[i]===')') {
@@ -68,9 +64,8 @@ const strChanger = (str)=>{
 }
 
 const checkRight = (str)=>{
-    console.log('check Right:',str)
     let left = 0,
-        right = 0;
+        right = 0,
         isRight = true;
     for (let i = 0;i<str.length;i++) {
         if (str[i]=== '(') {
@@ -88,4 +83,4 @@ const checkRight = (str)=>{
 // 2-1 . 올바르다 ? -> v대해 재귀 , 아니다 ? -> 4를 실행 => 문자열 조작 함수 필요 (strChanger)
 
 let str1 = '()))((()'
-console.log(solution(str1))
+console.log('답:',solution(str1));
