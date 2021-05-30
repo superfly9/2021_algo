f6658f24cd56053b22fb122246091d2f53b08593
superfly9 <superfly09@daum.net>
2021-05-30 17:34:17 +0900

    0530/programmers/L2/행렬의곱셈

--- programmers/L2/0528행렬의곱셈.js
+++ programmers/L2/0528행렬의곱셈.js
@@ -1,3 +1,6 @@
+// arr1의 열의 갯수 = arr2의 행의 갯수가 같아야 한다
+// 행 = arr.length  열 = arr[0].length
+
 let arr1 = 
     [
         [2, 3, 2], 
@@ -11,19 +14,39 @@ let arr1 =
         [2, 4, 1], 
         [3, 1, 1]
     ]
-// result = [[22, 22, 11], [36, 28, 18], [29, 20, 14]]
+// result = 
+// [
+//     [22, 22, 11],  i=0, j=0~arr2.length-1; k= 0~arr2[0].length - 1
+//     [36, 28, 18], 
+//     [29, 20, 14]
+// ]
 
+// //  ( 3, 2) * ( 2 ,5) = ( 3 ,5 )  =>
+// [
+//     [1,2,3,4,5]
+//     [1,2,3,4,4]
+//     [1,2,3,4,4]
+// ]
 
+// [ [1,2],         
+//   [5,6]
+//   [9,10] 
+// ]
+// [
+//     [1,2,3,4,5] j=0,k=0,j=1,k=0
+//     [3,5,3,8,9]
+// ]
 function solution(arr1, arr2) {
     let result = [];
     for (let i = 0;i<arr1.length;i++) {
         result.push([]);
-        let sum = 0;
-        for (let j = 0;j<arr2[i].length;j++) {
-            sum+= arr1[i][j] * arr2[j][i];
-            console.log('i:',i,'j:',j)
+        for (let j = 0;j<arr2[0].length;j++) {
+            let sum = 0;
+            for (let k=0;k<arr2.length;k++) {
+                sum+= arr1[i][k] * arr2[k][j];    
+            }
+        result[i].push(sum)    
         }
-        result[i].push(sum)
     }
     return result;
 }
