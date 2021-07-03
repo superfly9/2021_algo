93e2cd65f963995b2d4e153f00da47a039d8a3ad
superfly9 <superfly09@daum.net>
2021-07-03 17:37:16 +0900

    add sol2-가장큰정사각형

--- programmers/L2/0628가장큰정사각형.js
+++ programmers/L2/0628가장큰정사각형.js
@@ -37,3 +37,29 @@ function solution(board) {
 }
 
 console.log(solution(board));
+
+// sol2
+function solution(board) {
+  let max = 0;
+  let x = board.length;
+  let y = board[0].length;
+
+  if (x <= 1 || y <= 1) return 1;
+
+  for (let i = 1; i < x; i++) {
+    for (let j = 1; j < y; j++) {
+      if (board[i][j] >= 1) {
+        let min = Math.min(
+          board[i - 1][j],
+          board[i - 1][j - 1],
+          board[i][j - 1]
+        );
+
+        board[i][j] = min + 1;
+        max = Math.max(max, min + 1);
+      }
+    }
+  }
+
+  return max * max;
+}
